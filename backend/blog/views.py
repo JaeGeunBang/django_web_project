d77586375049d32487d5bb3c970c22c14bc1dee9
jaegeunbang <jgb710@naver.com>
2021-01-08 20:16:25 +0900

    DB내 Post 정보를 읽어 index.html에 노출 (FBV) - FBV는 함수에 기반을 둔 방법으로, 함수를 직접 만들어 원하는 기능을 직접   구현할수 있다.

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def index(request) :
+    #posts = Post.objects.all() # 데이터베이스에 쿼리를 날려 Post 관련 데이터를 가져온다.
+    posts = Post.objects.all().order_by('-pk')  # 최신 정보부터 가져오기
+
     return render(
         request,
         'blog/index.html',
+        {
+            'posts': posts,
+        }
     )
