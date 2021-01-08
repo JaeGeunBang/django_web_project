52242d189f670234a7137ec275e0f0cf8c264f86
jaegeunbang <jgb710@naver.com>
2021-01-08 20:53:26 +0900

    CBV 방식으로 구현 (FBV는 주석)

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page), # 정수 형태의 값을 pk 하는 변수로 담는다는 의미
-    path('', views.index) # blog/ 가장 기본 url
+    #path('<int:pk>/', views.single_post_page), # 정수 형태의 값을 pk 하는 변수로 담는다는 의미 (FBV 방식)
+    #path('', views.index) # blog/ 가장 기본 url (FBV 방식)
+    path('<int:pk>/', views.PostDetail.as_view()), # 정수 형태의 값을 pk 하는 변수로 담는다는 의미 (CBV 방식)
+    path('', views.PostList.as_view()) , # blog/ 가장 기본 url (CBV 방식)
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,26 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
-def index(request) :
-    #posts = Post.objects.all() # 데이터베이스에 쿼리를 날려 Post 관련 데이터를 가져온다.
-    posts = Post.objects.all().order_by('-pk')  # 최신 정보부터 가져오기
+### CBV 방식 (Class 방식)
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/single_post_page.html'
 
-def single_post_page(request, pk) :
-    post = Post.objects.get(pk=pk)
+### FBV 방식 (Function 방식)
+#def index(request) :
+#    #posts = Post.objects.all() # 데이터베이스에 쿼리를 날려 Post 관련 데이터를 가져온다.
+#    posts = Post.objects.all().order_by('-pk')  # 최신 정보부터 가져오기
 
-    return render (
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {
+#            'posts': posts,
+#        }
+#    )
+
+#def single_post_page(request, pk) :
+#    post = Post.objects.get(pk=pk)
+
+#    return render (
+#        request,
+#        'blog/single_post_page.html',
+#        {
+#           'post': post,
+#        }
+#    )
