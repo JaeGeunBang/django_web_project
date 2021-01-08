35e0d36a7abed90536771cab23266008264a6aa6
jaegeunbang <jgb710@naver.com>
2021-01-08 20:27:37 +0900

    상세 페이지 생성 - blog/에 포스트 번호를 추가 입력하면, 해당 포스트 번호의 page를 노출 - 또한, blog/에 title을 클릭하면 해당 포스트로 넘어감 - models.py에도 관련 함수를 개발해주어야 함

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('<int:pk>/', views.single_post_page), # 정수 형태의 값을 pk 하는 변수로 담는다는 의미
+    path('', views.index) # blog/ 가장 기본 url
 ]
