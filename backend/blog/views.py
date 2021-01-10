44a674d2e16796ccdb41b9f7374b76624aab212e
jaegeunbang <jgb710@naver.com>
2021-01-10 20:49:32 +0900

    category 블로그, 세부 블로그에 기능 추가하기

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 
 ### CBV 방식 (Class 방식)
@@ -9,9 +9,21 @@ class PostList(ListView):
     template_name = 'blog/index.html'
     ordering = '-pk'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 class PostDetail(DetailView):
     model = Post
     template_name = 'blog/single_post_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 ### FBV 방식 (Function 방식)
 #def index(request) :
