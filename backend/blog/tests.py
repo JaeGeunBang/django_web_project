d2d10b09ba8fd17004ee7901e6ae424ceef461ac
jaegeunbang <jgb710@naver.com>
2021-01-13 20:14:57 +0900

    [테스트] 포스트 목록에 포스트가 하나도 없는 경우 테스트 할것

--- backend/blog/tests.py
+++ backend/blog/tests.py
@@ -8,22 +8,41 @@ class TestView(TestCase):
         self.client = Client()
 
     def test_post_list(self):
-        # 블로그 리스트를 가져와 정상적으로 페이지 로드를 확인함
+        # 1.1 포스트 목록 페이지를 가져온다.
         response = self.client.get('/blog/')
+        # 1.2 정상적으로 페이지가 로드된다.
         self.assertEqual(response.status_code, 200)
-
-        # 페이지 타이블이 맞는지 확인
+        # 1.3 페이지 타이틀은 'blog' 이다
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-
-        # Blog, About Me 라는 문구가 네이게이션 바에 있는지 확인
+        # 1.4 네비게이션 바가 있다.
         navbar = soup.nav
+        # 1.5 Blog, About Me라는 문구가 네비게이션 바에 있다.
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
 
-        # 포스트가 하나도 없다면?
+        # 2.1 포스트가 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
+        # 2.2 main area에 '아직 게시물이 없습니다' 문구가 나타난다.
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
-        ## 등등..
+        # 3.1 포스트가 2개 있다면
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world.',
+        )
+        post_002 = Post.objects.create(
+            title='두 번째 포스트입니다.',
+            content='1등이 전부는 아니잖아요?',
+        )
+        # 3.2 포스트 목록 페이지를 새로고침 했을때
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 200)
+        # 3.3 main area에 포스트 2개의 제목이 존재한다.
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        # 3.4 아직 게시물이 없습니다 라는 문구는 더이상 나타나지 않는다.
+        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
