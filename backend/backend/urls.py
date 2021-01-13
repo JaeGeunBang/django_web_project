ef9109cb528b135160ff15a15972b0873bbfc631
jaegeunbang <jgb710@naver.com>
2021-01-13 20:53:11 +0900

    대문 페이지, about_me 페이지 추가

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
     path('accounts/', include('allauth.urls')),
 ]
 
